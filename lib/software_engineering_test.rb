c222dc13fedcb01a0d9f9df464d714920f6a932e
JopperTom <github@xintelx.com>
2015-03-30 04:16:22 -0700

    comments

--- lib/software_engineering_test.rb
+++ lib/software_engineering_test.rb
@@ -1,8 +1,8 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
 
 class SoftwareEngineeringTest
+  
+  # Let S be the set of numbers greater than zero and less than 100,000
+  # that are evenly divisible by 19.
   def initialize
     @slist = Array.new
     (0..10000).each do |s|
@@ -11,14 +11,21 @@ class SoftwareEngineeringTest
       end
     end
   end
+  
+  # How many numbers are there in S?
   def slist_ordinal
     slist.count
   end
+  
+   # How many numbers in S have a square that ends in a 1?    
    def sqr_ends_in_one
     slist.each do |s|
       if( /1$/ === (s**2).to_s )
     end
   end
+
+  # How many numbers in S have a reflection that is also in S? (The reflection
+  # of 145 is 541)   
  def reverses_equal
     slist.each do |s1|
         slist.each do |s2|
@@ -29,7 +36,10 @@ class SoftwareEngineeringTest
     end
   return ("count = count/2")
   end
-  def s_times_s_in_slist
+  
+ # How many numbers in S can be multiplied by some other number in S to produce 
+ # a third number in S?
+ def s_times_s_in_slist
     slist.each do |s1|
         slist.each do |s2|
           slist.each do |s3|
