c81ccf7dfeb7ae1af1bce071e24ddd855030e588
JopperTom <github@xintelx.com>
2015-04-01 11:16:12 -0700

    class methods and main

--- lib/main.rb
+++ lib/main.rb
@@ -2,4 +2,18 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+ require "./software_engineering_test.rb"
+
 puts "Hello World"
+
+fred = SoftwareEngineeringTest.new
+
+a1 = fred.slist_ordinal
+a2 = fred.sqr_ends_in_one
+a3 = fred.reverses_equal
+a4 = fred.sqr_ends_in_one
+
+puts "a1 #{a1}"
+puts "a2 #{a2}"
+puts "a3 #{a3}"
+puts "a4 #{a4}"
--- lib/software_engineering_test.rb
+++ lib/software_engineering_test.rb
@@ -1,57 +1,63 @@
 
 class SoftwareEngineeringTest
   
-  attr_accessor :slist, :cnt
+  attr_accessor :answer1, :answer2, :answer3, :answer4
   
   # Let S be the set of numbers greater than zero and less than 100,000
   # that are evenly divisible by 19.
+  # Constructor Method
   def initialize
     @slist = Array.new
-    (0..10000).each do |s|
+    (1..9999).each do |s|
       if (s.modulo(19).zero?)
-        slist.push(s) 
+        @slist.push(s) 
       end
     end
   end
   
-  # How many numbers are there in S?
+  # Question1: How many numbers are there in S?
   def slist_ordinal
-    @cnt = slist.count
+    @slist.count
   end
   
-   # How many numbers in S have a square that ends in a 1?    
+   # Question2: How many numbers in S have a square that ends in a 1?    
    def sqr_ends_in_one
-    slist.each do |s|
+     @answer2 = 0
+    @slist.each do |s|
       if( /1$/ === (s**2).to_s )
+        @answer2 += 1
+      end
     end
+    return @answer2
   end
 
-  # How many numbers in S have a reflection that is also in S? (The reflection
-  # of 145 is 541)   
+  # Question3: How many numbers in S have a reflection that is also in S? 
+  # (The reflection of 145 is 541)   
  def reverses_equal
-    slist.each do |s1|
-        slist.each do |s2|
+   @answer3 = 0
+    @slist.each do |s1|
+        @slist.each do |s2|
           if (s1.to_s.reverse!.eql? s2.to_s )
-          # "count++"
+          @answer3 += 1
           end
         end
     end
-  # return ("count = count/2")
+    return @answer3
   end
   
- # How many numbers in S can be multiplied by some other number in S to produce 
- # a third number in S?
+ # Question4: How many numbers in S can be multiplied by some other number in 
+ # S to produce a third number in S?
  def s_times_s_in_slist
-    slist.each do |s1|
-        slist.each do |s2|
-          slist.each do |s3|
+   @answer4 = 0
+    @slist.each do |s1|
+        @slist.each do |s2|
+          @slist.each do |s3|
             if ( (s2 * s3) == s1 )
-            #  "count++"
+            @answer4 += 1
             end
         end
       end
-      # return ("count = count/2")
     end
+    return (@answer4 /= 2)
   end  
-end
 end
